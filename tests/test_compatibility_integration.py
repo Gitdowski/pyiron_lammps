ab1fefc2baa5bd9f0360791eea981892358fa58b
Jan Janssen <jan-janssen@users.noreply.github.com>
2026-06-03 13:22:03 +0200

    Add integration test (#354)
    * Add integration test
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fixes
    
    * update
    
    * fixes
    
    * fixes
    
    * use default executable in test environment
    
    * fixes
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * update
    
    * openmpi fixes
    
    * fix mpi
    
    * fix pot
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ tests/test_compatibility_integration.py
@@ -0,0 +1,58 @@
+import os
+import unittest
+import shutil
+
+import numpy as np
+from ase.build import bulk
+from ase.constraints import FixAtoms
+
+from lammpsparser.compatibility.file import lammps_file_interface_function
+from lammpsparser.potential import get_potential_by_name
+
+try:
+    import lammps
+
+    skip_lammps_test = False
+except ImportError:
+    skip_lammps_test = True
+
+
+@unittest.skipIf(skip_lammps_test, "LAMMPS executable not available")
+class TestLammpsIntegration(unittest.TestCase):
+    def setUp(self):
+        self.static_path = os.path.abspath(
+            os.path.join("..", os.path.dirname(__file__), "static", "potential")
+        )
+        structure = bulk("Al", cubic=True)
+        c = FixAtoms(
+            indices=[atom.index for i, atom in enumerate(structure) if i % 2 == 0]
+        )
+        structure.set_constraint(c)
+        self.structure = structure
+        self.potential = get_potential_by_name(
+            potential_name="1999--Mishin-Y--Al--LAMMPS--ipr1",
+            resource_path=self.static_path,
+        )
+        self.working_directory = os.path.abspath("lmp_working_directory")
+
+    def tearDown(self):
+        if os.path.exists(self.working_directory):
+            shutil.rmtree(self.working_directory)
+
+    def test_lammps_integration(self):
+        shell_output, parsed_output, job_crashed = lammps_file_interface_function(
+            working_directory=self.working_directory,
+            structure=self.structure,
+            potential=self.potential,
+            calc_mode="md",
+            calc_kwargs={
+                "pressure": 0.0,
+                "temperature": 500.0,
+                "seed": 12345,
+                "n_ionic_steps": 1000,
+            },
+            units="metal",
+        )
+        self.assertFalse(job_crashed)
+        self.assertTrue(np.sum(parsed_output["generic"]["forces"][:, 0]) == 0.0)
+        self.assertTrue(np.sum(parsed_output["generic"]["forces"][:, 2]) == 0.0)
